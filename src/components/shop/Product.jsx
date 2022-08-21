041c95c175fb35bec946c1bfb02e68a604e01b76
fadugba oluwatosin <fadugbaoluwatosin@ukdion.local>
2022-08-21 14:15:45 +0100

    adding eventListener

--- src/components/shop/Product.jsx
+++ src/components/shop/Product.jsx
@@ -1,14 +1,21 @@
 import image16 from "../../assets/image16.png";
+import styles from "./shop.module.css"
 import image18 from "../../assets/group9.png"
 
 
-function Product(){
-    return <div className="sectionCard" >
-    <img src={image16} alt="" />
-      <h1>carrot</h1>
+function Product({ Product}) {
+  const {title, image, category, price, id} = Product;
+   const handleCick = () =>{
+    console.log(Product)
+   };
+
+
+    return <div className={styles.card} onClick={handleCick} >
+    <img src={image} className={styles.images} alt="" />
+      <h1 className={styles.title}>{title}</h1>
       <div className="div-image">
-      <img src={image18} alt="" />
-      <h6 className="h6">(3.2% Review)</h6>
+      <img src={image18} className={styles.star} alt="" />
+      <h6 className="header">(3.2% Review)</h6>
       </div>
       <button className="btn3"> order</button>
 </div>
--- src/pages/shop.jsx
+++ src/pages/shop.jsx
@@ -1,19 +1,33 @@
 import React, {useState, useEffect} from "react"
 import Header from "../components/common/Header"
 import Product from "../components/shop/Product"
+import styles from "../components/shop/shop.module.css"
 
 function Shop(){
 
-    let {product,setProduct} = useState([]);
-    
+    let [product,setProduct] = useState([]);
+
+    useEffect(function () {
+        fetch('https://fakestoreapi.com/products')
+        .then(res=>res.json())
+        .then(json=>{
+            console.log(json);
+            setProduct(json);
+        });
+    },[]);
+
     return(
         <section>
             
             <Header/>
-    
-        <div>
-            <Product/>
-        </div>
+            <div className={styles.container}>
+            {
+                product.length > 0 ? (
+                    product.map((product)=> (<Product Product = {product}/>
+                     ))
+                ) : ( <div>Loading...</div>)}
+           
+            </div> 
         </section>
     );
     
